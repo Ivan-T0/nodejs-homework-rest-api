5e9266fb843b9301d893043b3f81d3581b99c126
Иван-Т0 <vanyataran24@gmail.com>
2023-10-30 16:31:28 +0200

    update

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,7 +6,6 @@ const gravatar = require("gravatar");
 const path = require("path");
 const fs = require("fs/promises");
 const avatarsDir = path.join(__dirname, "../", "public", "avatars");
-console.log(avatarsDir);
 
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -64,7 +63,6 @@ const updateAvatar = async (req, res) => {
   const filename = `${_id}_${originalname}`;
   const resultUpload = path.join(avatarsDir, filename);
   await fs.rename(tempUpload, resultUpload);
-  console.log(filename);
   const avatarURL = path.join("avatars", filename);
   await User.findByIdAndUpdate(_id, { avatarURL });
   res.json({
